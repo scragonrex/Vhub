038f14c19eced000183a19a130ff713ce45d56b4
scragonrex <scragondragon@gmail.com>
2023-04-04 07:31:09 +0530

    Completed component 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Component1 from './components/Component1/Component1';
+import Component2 from './components/Component2/Component2';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
     <Navbar/>
     <Component1/>
+    <Component2/>
     </div>
   );
 }
---
+++ src/components/Component2/Component2.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import './Component2.css'
+const Component2 = () => {
+    return (
+        <div className='c2'>
+            <div className="c2s1">
+                <div className="c2s1cont">
+                <div className='c2h1'><p>Experience Ultimate Influencer Marketing Solution</p></div>
+                <div className='c2h2'><p>Reach your future customers organically through vHub.ai .</p></div>
+                <button className='btn'>Book a Demo</button>
+                </div>
+            </div>
+            <div className="c2s2">
+                <div className="c2card">
+                    <p style={{fontSize:"1.2rem"}}>Satisfied Brands</p>
+                    <h1>10<span style={{ color: "#22c55e" }}>+</span></h1>
+                    <div className="c2img" >
+                        <img src="https://vhub.ai/wp-content/uploads/2023/03/profile-2user.png" alt="" />
+                    </div>
+                </div>
+                <div className="c2card">
+                    <p style={{fontSize:"1.2rem"}}>Influencers Onboarded</p>
+                    <h1>20K<span style={{ color: "#22c55e" }}>+</span></h1>
+                    <div className="c2img" >
+                        <img src="https://vhub.ai/wp-content/uploads/2023/03/people.png" alt="" />
+                    </div>
+                </div>
+                <div className="c2card">
+                    <p style={{fontSize:"1.2rem"}}>Audience Reached</p>
+                    <h1>20M<span style={{ color: "#22c55e" }}>+</span></h1>
+                    <div className="c2img" >
+                        <img src="https://vhub.ai/wp-content/uploads/2023/03/timer.png" alt="" />
+                    </div>
+                </div>
+            </div>
+            <div className="c2s3">
+                <div className="c2card">
+                    <p style={{fontSize:"1.2rem"}}>Organic Impressions</p>
+                    <h1>50<span style={{ color: "#22c55e" }}>+</span></h1>
+                    <div className="c2img" >
+                        <img src="https://vhub.ai/wp-content/uploads/2023/03/flash-circle.png" alt="" />
+                    </div>
+                </div>
+                <div className="c2card">
+                    <p style={{fontSize:"1.2rem"}}>Influencers profiled</p>
+                    <h1>5M<span style={{ color: "#22c55e" }}>+</span></h1>
+                    <div className="c2img" >
+                        <img src="https://vhub.ai/wp-content/uploads/2023/03/profile-2user.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Component2;
