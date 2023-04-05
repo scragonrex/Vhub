60560ba942b4e641221e204a4f9499d0e70bd2f1
scragonrex <scragondragon@gmail.com>
2023-04-05 11:59:09 +0530

    added sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,35 @@
 import React from 'react'
 const Navbar = () => {
 
+  const handleClose = ()=>{
+    const element = document.getElementById('sidebar');
+    const opa = document.getElementById('opacity'); 
+    console.log(element);
+    element.style.width='0rem';
+    opa.style.width='0svw'
+  }
+
+  const handleOpen = ()=>{
+    const element = document.getElementById('sidebar');
+    const opa = document.getElementById('opacity'); 
+    opa.style.width='100vw'
+      element.style.width='50svw';
+  }
   return (
     <div className="navbar">
+      <div id='opacity' className="opacity"></div>
+      <div id='sidebar' className="sidebar">
+        <div className="closeIcon">
+        <i onClick={handleClose} class="fa-regular fa-2x fa-rectangle-xmark"></i>
+        </div>
+          <ul>
+          <li><a href="#c3">Why vhub</a></li>
+            <li><a href="#">Products</a></li>
+            <li><a href="#">Solution</a></li>
+            <li><a href="#">Pricing</a></li>
+            <li><a href="#">Resources</a></li>
+          </ul>
+      </div>
       <div className='navbarCont'>
         <div className="logo">
           <img src="https://vhub.ai/wp-content/uploads/2023/03/logo_png.png" alt="logo" />
@@ -21,7 +48,9 @@ const Navbar = () => {
           <button className='btn btnLogin'>Login</button>
           <button className='btn'>Book a demo</button>
         </div>
-        {/* <i class="fa-solid fa-2x fa-bars"></i> */}
+        <div className="menuIcon">
+        <i onClick={handleOpen} className="fa-solid fa-2x fa-bars"></i>
+        </div>
       </div>
     </div>
   )
